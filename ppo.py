44654fb8ce65d1742d9761c50528599ad1dfc864
Paul <P.Chudzik@stud.elka.pw.edu.pl>
2018-05-03 15:18:27 +0200

    add gae and batches to ppo

--- ppo.py
+++ ppo.py
@@ -25,41 +25,48 @@ class Net(nn.Module):
 env = gym.make('CartPole-v0')
 
 n_epochs = 5000
-n_iters = 3
+n_iters = 4
 gamma = 0.99
-eps = 0.1
+lam = 0.95
+eps = 0.2
+T = 512
+num_batches = 16
+batch_size = T // num_batches
 
 model = Net(64)
-optimizer = optim.Adam(model.parameters()) 
+optimizer = optim.Adam(model.parameters(), lr=2.5e-4)
 
-def discount(rewards):
+def advantages(rewards, values, dones):
     summed_reward = 0
-    discounted = [0] * len(rewards)
+    returns = [0] * len(rewards)
 
     for i in reversed(range(len(rewards))):
-        summed_reward = summed_reward * gamma + rewards[i]
-        discounted[i] = summed_reward
+        delta = rewards[i] + gamma * (1 - dones[i]) * values[i + 1] - values[i]
+        summed_reward = summed_reward * (1 - dones[i]) * gamma * lam + delta
+        returns[i] = summed_reward
 
-    return discounted
+    return returns
 
 def var(x):
     return Variable(torch.from_numpy(x).float())
 
 total_rewards = deque(maxlen=100)
+observation = env.reset()
+current_reward = 0
+num_eps = 0
+
 for i_episode in range(n_epochs):
     old_probs = []
     actions = []
     states = []
     rewards = []
     values = []
-    
-    observation = env.reset()
-    optimizer.zero_grad()
+    dones = []
 
-    for t in range(200):
-        if i_episode % 10 == 0:
+    for t in range(T):
+        if num_eps % 50 == 0:
             env.render()
-        
+
         probs, value = model(var(observation))
         states.append(observation)
 
@@ -72,37 +79,54 @@ for i_episode in range(n_epochs):
         observation, reward, done, info = env.step(action[0])
         rewards.append(reward)
         values.append(value.data[0])
-          
+        dones.append(done)
+        current_reward += reward
+
         if done:
-            total_reward = sum(rewards)
-            total_rewards.append(total_reward)
+            num_eps += 1
+            total_rewards.append(current_reward)
 
-            if i_episode % 10 == 0:
-                print("Episode: {0}, Score: {1}, Last 100 mean: {2}".format(
-                    i_episode, total_reward, np.mean(total_rewards)))
+            current_reward = 0
+            observation = env.reset()
+    
+    _, value = model(var(observation))
+
+    advs = np.vstack(advantages(rewards, values + [value.data[0]], dones))
+    returns = np.vstack(values) + np.copy(advs)
+    advs = (advs - advs.mean()) / (advs.std() + 1e-8)
+
+    states = np.vstack(states)
+    old_probs = np.vstack(old_probs)
+
+    batch_range = list(range(batch_size))
+
+    for n in range(n_iters):
+        idx = np.random.permutation(T)
         
-            rewards = np.vstack(discount(rewards))
-            states = np.vstack(states)
-            old_probs = np.vstack(old_probs)
-            values = np.vstack(values)
-   
-            advantages = rewards - values
-            batch = list(range(len(actions)))
+        for num_batch in range(num_batches):
+            b_idx = idx[num_batch * batch_size : (num_batch + 1) * batch_size]
 
-            for n in range(n_iters):
-                optimizer.zero_grad()
+            b_advs = np.copy(advs[b_idx])
+            b_returns = np.copy(returns[b_idx])
+            b_old_probs = np.copy(old_probs[b_idx])
+            b_states = np.copy(states[b_idx])
+            b_actions = np.copy(np.array(actions)[b_idx])
 
-                probs, values = model(var(states))
-                r = torch.exp(torch.log(probs[batch, actions]) - var(old_probs).squeeze(1))
-                A = var(advantages).squeeze(1)
+            optimizer.zero_grad()
 
-                L_policy = torch.min(r * A, r.clamp(1 - eps, 1 + eps) * A)
-                L_value = (values - var(rewards)) ** 2
-                L_entropy = -((probs * torch.log(probs)).sum(1))
-  
-                L = -(L_policy - L_value + 0.01 * L_entropy)
-                
-                L.sum().backward()
-                optimizer.step()
+            probs, values = model(var(b_states))
+            r = torch.exp(torch.log(probs[batch_range, b_actions]) - var(b_old_probs).squeeze(1))
+            A = var(b_advs).squeeze(1)
 
-            break
+            L_policy = -torch.min(r * A, r.clamp(1 - eps, 1 + eps) * A).mean()
+            L_value = (values - var(b_returns)).pow(2).mean()
+            L_entropy = (-((probs * torch.log(probs)).sum(-1))).mean()
+
+            L = L_value + L_policy - 0.01 * L_entropy
+            
+            L.backward()
+            optimizer.step()
+
+    if i_episode % 10 == 0:
+        print("Episode: {0}, Last 100 mean: {1}".format(
+            i_episode, np.mean(total_rewards)))
